db087d53ae276b62ec3fe86f99bd30ea83562239
jerhum <jerhum@msn.com>
2025-03-09 21:56:17 +0100

    changement langue FR

--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,27 +1,26 @@
 const TMDB_KEY = process.env.REACT_APP_TMDB_KEY;
 const TMDB_BASE_URL = process.env.REACT_APP_TMDB_BASE_URL;
-console.log(TMDB_KEY);
 
 const fetchMoviePopular = async () => {
-  return await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_KEY}`).then(
+  return await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_KEY}&language=fr-FR`).then(
     (reponse) => reponse.json()
   );
 };
 
 const fetchMovieNowPlaying = async () => {
-  return await fetch(`${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_KEY}`).then(
+  return await fetch(`${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_KEY}&language=fr-FR`).then(
     (reponse) => reponse.json()
   );
 };
 
 const fetchMovieTopRated = async () => {
-  return await fetch(`${TMDB_BASE_URL}/movie/top_rated?api_key=${TMDB_KEY}`).then(
+  return await fetch(`${TMDB_BASE_URL}/movie/top_rated?api_key=${TMDB_KEY}&language=fr-FR`).then(
     (reponse) => reponse.json()
   );
 };
 
 const fetchMovieUpcoming = async () => {
-  return await fetch(`${TMDB_BASE_URL}/movie/upcoming?api_key=${TMDB_KEY}&language=en-US&page=1`).then(
+  return await fetch(`${TMDB_BASE_URL}/movie/upcoming?api_key=${TMDB_KEY}&language=fr-FR&page=1`).then(
     (reponse) => reponse.json()
   );
 };
