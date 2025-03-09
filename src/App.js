535296206dc2e9e373b69fd1adb68f4e3002a8b4
jerhum <jerhum@msn.com>
2025-03-09 21:29:33 +0100

    ajout page details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="appContainer">
       <Header />
-      <Outlet/>
+      <Outlet />
     </div>
   );
 }
